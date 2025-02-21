80e5fa0b5aedec106359557d387b4042c556a0a3
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-02-21 11:39:03 -0500

    add boot_dummy script

---
+++ boot_script/boot_dummy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# fake it for now
+PYSURFHSK="sleep infinity"
+
+# dead duplicate of what's in pueo-squashfs
+catch_term() {
+    echo "termination signal caught"
+    kill -TERM "$waitjob" 2>/dev/null
+}
+
+# automatically program the FPGA, weee!
+# we're not there yet, need to update stuff
+#autoprog.py pysoceeprom.PySOCEEPROM
+
+# here's where pysurfHskd would run
+$PYSURFHSK &
+waitjob=$!
+
+wait $waitjob
+RETVAL=$?
+
+exit $RETVAL
