669338c3e59427b19b4b7a29525b5311cc3e26f3
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-03-18 16:39:52 -0400

    test forwarding

--- pyturfHskd/test.py
+++ pyturfHskd/test.py
@@ -3,6 +3,7 @@ import struct
 import selectors
 import signal
 import logging
+import queue
 
 from signalhandler import SignalHandler
 from turfSerHandler import SerHandler
@@ -43,35 +44,72 @@ logging.basicConfig(level=10)
 sel = selectors.DefaultSelector()
 handler = SignalHandler(sel)
 
-# spawn the upstream serial handler for testing
-upHskSerial = SerHandler(sel,
-                         logName=LOG_NAME,
-                         port='/dev/ttySC0',
-                         baud=460800)
-# now let's try adding the Ethernet upstream 
-upHskEth = SerHandler(sel,
-                      logName=LOG_NAME,
-                      port='/dev/hskspi')
+# let's collect our upstream/downstream interfaces
+upstreams = []
+downstreams = []
 
-def upHskEthHandler(fd, mask):
-    if upHskEth.fifo.empty():
-        logger.error("handler called but FIFO is empty?")
-        return
-    pktno = os.read(fd, 1)
-    pkt = upHskEth.fifo.get()
-    logger.info("got packet #%d from upHskEth:%s", pktno[0], pkt.hex(sep=' '))
+# create an upstream-to-downstream FIFO
+packetsForDownstream = queue.Queue()
+# create a downstream-to-upstream FIFO
+packetsForUpstream = queue.Queue()
 
-def upHskSerialHandler(fd, mask):
-    if upHskSerial.fifo.empty():
-        logger.error("handler called but FIFO is empty?")
-        return
-    pktno = os.read(fd, 1)
-    pkt = upHskSerial.fifo.get()
-    logger.info("got packet #%d from upHskSerial:%s", pktno[0], pkt.hex(sep=' '))
+# create the handlers. We added the name parameter to just
+# make things a bit easier to factor and debug
 
-upHskSerial.start(callback=upHskSerialHandler)
-upHskEth.start(callback=upHskEthHandler)
+# true serial upstreams
+for i in range(2):
+    upstreams.append( SerHandler(sel,
+                                 name="HSK"+str(i),
+                                 logName=LOG_NAME,
+                                 port='/dev/ttySC'+str(i),
+                                 baud=460800) )
+# ethernet upstream fakey serial
+upstreams.append( SerHandler(sel,
+                             name="SFC",
+                             logName=LOG_NAME,
+                             port='/dev/hskspi') )
 
+# true serial downstreams
+for i in range(4):
+    downstreams.append( SerHandler(sel,
+                                   name="TURFIO"+str(i),
+                                   logName=LOG_NAME,
+                                   port='/dev/ttyUL'+str(i),
+                                   baud=500000) )
+
+# start the upstreams
+for uh in upstreams:
+    # create a closure as the callback function
+    def upstreamHandler(fd, mask):
+        if uh.fifo.empty():
+            logger.error("handler called but FIFO is empty?")
+            return
+        pktno = os.read(fd, 1)
+        pkt = uh.fifo.get()
+        packetsForDownstream.put(pkt)
+        logger.info("got upstream packet #%d from %s via %s: %s",
+                    pktNo[0],
+                    hex(pkt[0]),
+                    uh.name,
+                    pkt.hex(sep=' '))
+    uh.start(callback=upstreamHandler)
+# start the downstreams
+for dh in downstreams:
+    # create a closure as the callback function
+    def downstreamHandler(fd, mask):
+        if dh.fifo.empty():
+            logger.error("handler called but FIFO is empty?")
+            return
+        pktno = os.read(fd, 1)
+        pkt = dh.fifo.get()
+        packetsForUpstream.put(pkt)
+        logger.info("got downstream packet #%d from %s via %s: %s",
+                    pktNo[0],
+                    hex(pkt[0]),
+                    uh.name,
+                    pkt.hex(sep=' '))
+    dh.start(callback=downstreamHandler)
+        
 while not handler.terminate:
     events = sel.select()
     for key, mask in events:
@@ -85,6 +123,23 @@ while not handler.terminate:
             logger.error(traceback.format_exc())
             handler.set_terminate()
 
+    # HOUSEKEEPING ROUTER!!
+    while not packetsForDownstream.empty():
+        pkt = packetsForDownstream.get()
+        # just effing broadcast stuff downstream for now, we'll
+        # worry about routing later.
+        for dh in downstreams:
+            dh.sendPacket(pkt)
+    while not packetsForUpstream.empty():
+        pkt = packetsForUpstream.get()
+        logger.info("trying to find an upstream for destination %s",
+                    hex(pkt[0]))
+        for uh in upstreams:
+            if pkt[0] in uh.sources:
+                logger.info("forwarding packet to %s",
+                            uh.name)
+                uh.sendPacket(pkt)
+                
 logger.info("Terminating!")
 upHskSerial.stop()
 
