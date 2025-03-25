802908e9976f7bdc0bcb5cdb8c47c948dd35ce28
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-03-24 22:58:11 -0400

    add the make_pyturfhskd.sh script

---
+++ pyturfHskd/make_pyturfhskd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# this script builds the desired portion of pyturfHskd
+# in a target.
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+MAIN_FILE="pyturfHskd.py"
+
+AUX_FILES="pueoTimer.py \
+	   turfHskHandler.py \
+	   turfHskProcessor.py \
+           turfStartupHandler.py"
+
+if [ "$#" -ne 1 ] ; then
+    echo "usage: make_pyturfhskd.sh <destination directory>"
+    echo "usage: (e.g. make_pyturfhskd.sh path/to/tmpsquashfs/pylib/ )"
+    exit 1
+fi
+
+DEST=$1
+mkdir -p $DEST/pyturfHskd
+
+cp ${SCRIPT_DIR}/${MAIN_FILE} $DEST/pyturfHskd/
+for f in ${AUX_FILES} ; do
+    cp ${SCRIPT_DIR}/$f $DEST/pyturfHskd/
+done
