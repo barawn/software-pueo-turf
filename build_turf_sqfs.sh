c0f3770a2954c5f3f55a6da9bdc85007885a53ca
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-06-13 13:57:42 -0400

    include pynmea2

--- build_turf_sqfs.sh
+++ build_turf_sqfs.sh
@@ -40,7 +40,8 @@ PYTHON_SINGLE_FILES="pueo-utils/pysoceeprom/pysoceeprom.py \
 # multi-file python modules wrapped in directories
 PYTHON_DIRS="hskSpiBridge/ \
 	     hskRouter/ \
-	     pyturfGpsd/"
+	     pyturfGpsd/ \
+	     pynmea2/pynmea2/"
 
 # scripts
 SCRIPTS="pueo-utils/scripts/build_squashfs \
